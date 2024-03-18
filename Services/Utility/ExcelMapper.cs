2c9778ea86bba26290ea90137e65d60d04444b23
Aderibigbe Adeola <adeola.aderibigbe@cyphercrescent.com>
2024-03-18 17:10:23 +0100

    Change project structure

--- Services/Utility/ExcelMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data;
-
-namespace Excelify.Services.Utility
-{
-    public abstract class ExcelMapper : IExcelMapper
-    {
-        public abstract Task<List<T>> Map<T>(IEnumerable<DataRow> rows);
-    }
-}
--- Services/Utility/IExcelMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data;
-
-namespace Excelify.Services.Utility
-{
-    public interface IExcelMapper
-    {
-        Task<List<T>> Map<T>(IEnumerable<DataRow> rows);
-    }
-}
